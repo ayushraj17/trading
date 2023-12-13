f76379cd239a041853da78f11b4294dc2ce8780c
ayushraj17 <meayushraj17@gmail.com>
2023-12-13 11:41:17 +0530

    init charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+"use client";
+import { Line } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	Tooltip,
+	PointElement,
+	LineElement,
+} from "chart.js";
+import { useEffect, useState } from "react";
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Tooltip
+);
+import data from "../data";
+import { useLocalStorage } from "usehooks-ts";
+const PublicGoogleSheetsParser = require("public-google-sheets-parser");
+const spreadSheetId = "19Obu4SoUuaTdFvVwoCFcLvl0p_DoyE4YTpg44gVQjsU";
+const parser = new PublicGoogleSheetsParser(spreadSheetId);
+
+type MarketData = {
+	"Too many SOLID greens together is NOT good & vice versa Date How to read MB ": string;
+	"Number of Stocks up 4.5%+ Today A: Study market behaviour around extreme figures. There is a clear pattern everytime. Markets tend to stall & continue or stall & reverse the direction ": number;
+	"Number of stocks down 4.5%+ Today ": number;
+	"Number of stocks up 20%+ in 5 days ": number;
+	"HEAT MAP Number of stocks down 20%+ in 5 days ": number;
+	"Number of Stocks above 20 DMA (Early Signal) ": number;
+	"Number of Stocks below 20 DMA (Early Signal) ": number;
+	"Number of Stocks above 50 DMA ": number;
+	"Number of Stocks below 50 DMA ": number;
+};
+const keys: (keyof MarketData)[] = [
+	"Too many SOLID greens together is NOT good & vice versa Date How to read MB ",
+	"Number of Stocks up 4.5%+ Today A: Study market behaviour around extreme figures. There is a clear pattern everytime. Markets tend to stall & continue or stall & reverse the direction ",
+	"Number of stocks down 4.5%+ Today ",
+	"Number of stocks up 20%+ in 5 days ",
+	"HEAT MAP Number of stocks down 20%+ in 5 days ",
+	"Number of Stocks above 20 DMA (Early Signal) ",
+	"Number of Stocks below 20 DMA (Early Signal) ",
+	"Number of Stocks above 50 DMA ",
+	"Number of Stocks below 50 DMA ",
+];
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [currentTab, setCurrentTab] = useState<keyof MarketData>(
+		"Number of Stocks above 20 DMA (Early Signal) "
+	);
+	const [marketData, setMarketData] = useLocalStorage<MarketData[]>(
+		"marketData",
+		[]
+	);
+	useEffect(() => {
+		parser.parse().then((items: any) => {
+			setMarketData(items.reverse());
+		});
+	}, [setMarketData]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	return (
+		<div className="m-10">
+			<select
+				className="mb-6"
+				value={currentTab}
+				onChange={(e) => setCurrentTab(e.target.value as keyof MarketData)}
+			>
+				{keys.map((key) => (
+					<option key={key} value={key}>
+						{key}
+					</option>
+				))}
+			</select>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			<Line
+				data={{
+					labels: Object.values(marketData).map(
+						(i) =>
+							i[
+								"Too many SOLID greens together is NOT good & vice versa Date How to read MB "
+							]
+					),
+					datasets: [
+						{
+							backgroundColor: Object.values(marketData).map((i) => {
+								if (Number(i[currentTab]) < 400) {
+									return "lime";
+								}
+								if (Number(i[currentTab]) > 1200) {
+									return "red";
+								}
+								return "gray";
+							}),
+							// label: "Four Five",
+							data: Object.values(marketData).map((i) => i[currentTab]),
+						},
+					],
+				}}
+			/>
+		</div>
+	);
 }
